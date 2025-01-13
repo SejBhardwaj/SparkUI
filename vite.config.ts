d345c171fa997cce9099f39aba8569bd8b33d76d
SejalBhardwaj20 <BHARDWAJSEJAL20@GMAIL.COM>
2025-01-14 00:11:00 +0530

    config: setup Vite build configuration

---
+++ vite.config.ts
@@ -0,0 +1,31 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@components': path.resolve(__dirname, './src/components'),
+      '@utils': path.resolve(__dirname, './src/utils'),
+      '@hooks': path.resolve(__dirname, './src/hooks'),
+      '@types': path.resolve(__dirname, './src/types'),
+    },
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom'],
+        },
+      },
+    },
+    sourcemap: true,
+    minify: 'terser',
+  },
+  server: {
+    port: 5173,
+    open: true,
+  },
+});
