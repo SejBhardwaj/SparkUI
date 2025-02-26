c611d4c49496a4629fa0294cedc9fe0df617533e
SejalBhardwaj20 <BHARDWAJSEJAL20@GMAIL.COM>
2025-02-27 00:33:00 +0530

    design: create design system tokens index

---
+++ src/design-system/index.ts
@@ -0,0 +1,35 @@
+/**
+ * Design System Tokens Index
+ * Central export for all design tokens
+ */
+
+// Colors
+export * from './colors/primary';
+export * from './colors/secondary';
+export * from './colors/semantic';
+export * from './colors/neutral';
+export * from './colors/utilities';
+
+// Typography
+export * from './typography/fonts';
+export * from './typography/sizes';
+export * from './typography/weights';
+export * from './typography/line-heights';
+export * from './typography/letter-spacing';
+export * from './typography/utilities';
+export * from './typography/presets';
+export * from './typography/responsive';
+
+// Spacing
+export * from './spacing/scale';
+export * from './spacing/utilities';
+
+// Layout
+export * from './layout/grid';
+export * from './layout/containers';
+export * from './layout/utilities';
+
+// Animations
+export * from './animations/timings';
+export * from './animations/transitions';
+export * from './animations/utilities';
