31a4d0b7c92bd63626b55d458ec7978fcddd20e1
SejalBhardwaj20 <BHARDWAJSEJAL20@GMAIL.COM>
2025-07-07 01:08:00 +0530

    feat: create card body and content area

---
+++ src/components/data-display/Card/CardBody.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+export interface CardBodyProps {
+  children: React.ReactNode;
+}
+export const CardBody: React.FC<CardBodyProps> = ({ children }) => (
+  <div className="card-body">{children}</div>
+);
