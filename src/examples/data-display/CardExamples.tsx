e48edf84cda76a9852ae33e7792b330c0704fc68
SejalBhardwaj20 <BHARDWAJSEJAL20@GMAIL.COM>
2025-07-09 01:48:00 +0530

    example: create card usage examples

---
+++ src/examples/data-display/CardExamples.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+import { Card } from '../../components/data-display/Card/Card';
+export const CardExamples: React.FC = () => (
+  <div><h1>Card Examples</h1><Card>Example card content</Card></div>
+);
