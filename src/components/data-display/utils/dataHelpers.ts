3873c01a00e7722d4af94746328f3eb1a644c6cb
SejalBhardwaj20 <BHARDWAJSEJAL20@GMAIL.COM>
2025-07-24 01:26:00 +0530

    feat: create data manipulation helpers

---
+++ src/components/data-display/utils/dataHelpers.ts
@@ -0,0 +1,8 @@
+export const groupBy = <T>(array: T[], key: keyof T): Record<string, T[]> => {
+  return array.reduce((result, item) => {
+    const groupKey = String(item[key]);
+    if (!result[groupKey]) result[groupKey] = [];
+    result[groupKey].push(item);
+    return result;
+  }, {} as Record<string, T[]>);
+};
