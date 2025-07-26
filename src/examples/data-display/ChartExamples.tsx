aba1ece35a4666545fa2d3ebd27da146e9b33dce
SejalBhardwaj20 <BHARDWAJSEJAL20@GMAIL.COM>
2025-07-27 00:52:00 +0530

    example: create chart usage examples

---
+++ src/examples/data-display/ChartExamples.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+import { LineChart } from '../../components/data-display/Charts/LineChart';
+export const ChartExamples: React.FC = () => (
+  <div><h1>Chart Examples</h1></div>
+);
