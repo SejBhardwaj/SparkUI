82ac1543a48cd24c00c10c8d523f07e01b8f8b62
SejalBhardwaj20 <BHARDWAJSEJAL20@GMAIL.COM>
2025-07-27 01:47:00 +0530

    example: create statistics usage examples

---
+++ src/examples/data-display/StatisticsExamples.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+import { Stat } from '../../components/data-display/Statistics/Stat';
+export const StatisticsExamples: React.FC = () => (
+  <div><h1>Statistics Examples</h1></div>
+);
