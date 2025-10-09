e071921815cceb63a9adc115ab5067b497bb8eff
SejalBhardwaj20 <BHARDWAJSEJAL20@GMAIL.COM>
2025-10-10 01:58:00 +0530

    example: add drawer component examples

---
+++ src/examples/feedback/DrawerExamples.tsx
@@ -0,0 +1,9 @@
+﻿// Drawer Component Examples
+export const DrawerExamples = () => {
+  return (
+    <div>
+      <h2>Drawer Examples</h2>
+      <p>Examples of drawer components in various configurations</p>
+    </div>
+  );
+};
