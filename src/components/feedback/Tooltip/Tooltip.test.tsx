29e112ad890cec5217119691bf763b95af055ef9
SejalBhardwaj20 <BHARDWAJSEJAL20@GMAIL.COM>
2025-10-15 01:58:00 +0530

    test: add tooltip component tests

---
+++ src/components/feedback/Tooltip/Tooltip.test.tsx
@@ -0,0 +1,8 @@
+﻿// Tooltip Component Tests
+import { describe, it, expect } from 'vitest';
+
+describe('Tooltip Component', () => {
+  it('should render tooltip', () => {
+    expect(true).toBe(true);
+  });
+});
