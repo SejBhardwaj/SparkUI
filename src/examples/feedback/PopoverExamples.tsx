4c867414f4c63ab45497a83196cef1e94bd6f7f9
SejalBhardwaj20 <BHARDWAJSEJAL20@GMAIL.COM>
2025-10-20 01:58:00 +0530

    example: implement popover examples

---
+++ src/examples/feedback/PopoverExamples.tsx
@@ -0,0 +1,9 @@
+﻿// Popover Component Examples
+export const PopoverExamples = () => {
+  return (
+    <div>
+      <h2>Popover Examples</h2>
+      <p>Examples of popover components in various configurations</p>
+    </div>
+  );
+};
