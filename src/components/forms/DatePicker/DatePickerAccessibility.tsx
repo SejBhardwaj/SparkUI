fc5b989c68b2c6f3a5cf646812e0815afad06aa9
SejalBhardwaj20 <BHARDWAJSEJAL20@GMAIL.COM>
2025-11-04 01:47:00 +0530

    feat: implement date picker accessibility features

---
+++ src/components/forms/DatePicker/DatePickerAccessibility.tsx
@@ -0,0 +1,8 @@
+﻿// Date Picker Accessibility Features
+export const DatePickerAccessibility = {
+  role: 'dialog',
+  ariaLabel: 'date-picker',
+  keyboardNavigation: true,
+  screenReaderSupport: true,
+  focusManagement: true
+};
