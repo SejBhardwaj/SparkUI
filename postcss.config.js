db13a191196034e8b10b9e98da0773acd6e78218
SejalBhardwaj20 <BHARDWAJSEJAL20@GMAIL.COM>
2026-01-06 01:37:00 +0530

    perf: optimize CSS with PurgeCSS

--- postcss.config.js
+++ postcss.config.js
@@ -4,3 +4,13 @@ export default {
     autoprefixer: {},
   },
 };
+
+// PurgeCSS configuration
+module.exports = {
+  plugins: {
+    '@fullhuman/postcss-purgecss': {
+      content: ['./src/**/*.{js,jsx,ts,tsx}', './index.html'],
+      safelist: ['html', 'body'],
+    },
+  },
+};
