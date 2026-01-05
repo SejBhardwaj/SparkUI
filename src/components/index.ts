bc3035093affc0c132d52fc2f03d4ec6a2c63121
SejalBhardwaj20 <BHARDWAJSEJAL20@GMAIL.COM>
2026-01-06 00:19:00 +0530

    perf: optimize component barrel exports

---
+++ src/components/index.ts
@@ -0,0 +1,9 @@
+// Optimized barrel exports with tree-shaking support
+export * from './feedback';
+export * from './forms';
+export * from './data-display';
+export * from './layout';
+export * from './navigation';
+
+// Type exports
+export type * from './types';
