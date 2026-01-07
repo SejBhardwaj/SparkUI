74b6b517946d63508083d3cfe90fe45c761df9e4
SejalBhardwaj20 <BHARDWAJSEJAL20@GMAIL.COM>
2026-01-08 00:27:00 +0530

    feat: create unified component library exports

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+// UI Components Library - Main Entry Point
+// Version 1.0.0
+
+// Components
+export * from './components';
+
+// Hooks
+export * from './hooks';
+
+// Utils
+export * from './utils';
+
+// Types
+export * from './types';
+
+// Theme
+export * from './theme';
+
+// Design System
+export * from './design-system';
